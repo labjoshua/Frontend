d4cf0aac718b777daf14c52d5663d17ea270d218
Joshua <lab.joshacc@gmail.com>
2023-09-15 16:10:15 +0800

    Unfinished login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './style.css'
 
 function Login() {
   return (
-    <div className="login-container">
-      <p className="login-font">Login</p>
+    <div className='background-image'>
+      <div className='overlay'>
+        <h1>Login</h1>
+
+        <div className='input-box'>
+          <input type='text' placeholder='Username' required></input>
+        </div>
+
+        <div className='input-box'>
+          <input type='password' placeholder='Password' required></input>
+        </div>
+
+        <div className='remember-forgot'>
+          <label><input type='checkbox'>Rememebr me</input></label>
+          
+        </div>
+
+        <button type='submit' class='btn'>Login</button>
+      </div>
       </div>
   )
 }
