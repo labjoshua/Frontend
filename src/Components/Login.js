7326261edbf5445ebecdc8a7894f2ddfa3dcee9a
Joshua <lab.joshacc@gmail.com>
2023-09-15 15:21:59 +0800

    Starting

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login-container">
+      <p className="login-font">Login</p>
+      </div>
   )
 }
 
