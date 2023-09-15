7a239198d13e4ba4e3bcf0a3a7b9cb8cbd0b0c0c
Joshua <lab.joshacc@gmail.com>
2023-09-15 16:22:41 +0800

    Another commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
 
 function Login() {
   return (
@@ -16,14 +16,25 @@ function Login() {
         </div>
 
         <div className='remember-forgot'>
-          <label><input type='checkbox'>Rememebr me</input></label>
-          
+          <label>
+            <input type='checkbox' />
+            Remember me
+          </label>
+          <a href='#'>Forgot Password</a>
         </div>
 
-        <button type='submit' class='btn'>Login</button>
+        <button type='submit' className='btn'>
+          Login
+        </button>
+
+        <div className='register-link'>
+          <p>Don't have an account?
+            <a href='#'>Register</a>
+          </p>
+        </div>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Login
+export default Login;
