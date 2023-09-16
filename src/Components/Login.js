dd253524c773445b80e20b82781eb1b802d3f58e
Joshua <lab.joshacc@gmail.com>
2023-09-16 20:26:46 +0800

    Progress

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,43 +2,27 @@ import React from 'react';
 import './style.css';
 
 function Login() {
-  return (
-    <div className='background-image'>
-      <div className='overlay'>
-
-        <div className='Login-Form'>
-        <h1 className='Login-Form'>Login</h1>
-        <div className='input-box'>
-          <input type='text' placeholder='Username' required></input>
-          <i class='bx bxs-user'></i>
-        </div>
-
-        <div className='input-box'>
-          <input type='password' placeholder='Password' required></input>
-          <i class='bx bxs-lock-alt'></i>
-        </div>
-
-        <div className='remember-forgot'>
-          <label>
-            <input type='checkbox' />
-            Remember me
-          </label>
-          <a href='#'>Forgot Password</a>
-        </div>
-
-        <button type='submit' className='btn'>
-          Login
-        </button>
-
-        <div className='register-link'>
-          <p>Don't have an account?
-            <a href='#'>Register</a>
-          </p>
-        </div>
-        </div>
+  return(
+    <div className='wrapper'>
+      <h1>Login</h1>
+      <div className='input-box'>
+        <input type='text' placeholder='Username' required></input>
+        <i className='bx bxs-user'></i>
+      </div>
+      <div className='input-box'>
+        <input type='password' placeholder='Password' required></input>
+        <i className='bx bxs-lock-alt password-icon'></i>
+      </div>
+      <div className='remember-forgot'>
+        <label><input type='checkbox'></input>Remember me</label>
+        <a href='#'>Forgot Password</a>
+      </div>
+      <button type='submit' className='btn'>Login</button>
+      <div className='register-link'>
+        <p>Don't have an account? <a href='#'>Register</a></p>
       </div>
     </div>
-  );
+  )
 }
 
 export default Login;
