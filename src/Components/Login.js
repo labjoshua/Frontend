34b950cc3896e36a60d5855c2f64a3ebe86594ca
Joshua <lab.joshacc@gmail.com>
2023-09-17 01:45:36 +0800

    Finished login design, but still unresponsive on mobile device

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './style.css';
+import loginImage from './Assests/arthurs-place-anilao.png';
 
 function Login() {
   return(
     <div className='wrapper'>
+      <div>
+        <img src ={loginImage} alt='Arthur Logo'></img>
+      </div>
       <h1>Login</h1>
       <div className='input-box'>
         <input type='text' placeholder='Username' required></input>
-        <i className='bx bxs-user'></i>
       </div>
       <div className='input-box'>
         <input type='password' placeholder='Password' required></input>
-        <i className='bx bxs-lock-alt password-icon'></i>
       </div>
       <div className='remember-forgot'>
         <a href='#'>Forgot Password</a>
