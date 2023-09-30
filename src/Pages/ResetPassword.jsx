0e96c7b164740c7706a33c93af0f5f3b766a4233
Joshua <lab.joshacc@gmail.com>
2023-10-01 03:01:01 +0800

    Just need to optimize the update info

--- src/Pages/ResetPassword.jsx
+++ src/Pages/ResetPassword.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
-import "./pagesStyle.css"
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import "./pagesStyle.css";
+import { Link, useNavigate } from 'react-router-dom';
 
 const ResetPassword = () => {
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleUpdatePassword = async (e) => {
+        e.preventDefault();
+
+        // Retrieve guestID from local storage
+        const guestID = localStorage.getItem('userID');
+
+        try {
+            const response = await fetch('/Components/UpdatePassword', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userPass: newPassword, guestID }),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                // Password updated successfully, you might want to handle this in your UI
+                console.log(data.message);
+                navigate('/'); // Redirect to login page after successful password update
+            } else {
+                // Password update failed, handle error or show a message to the user
+                console.error(data.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            // Handle network or other errors here
+        }
+    };
+
     return (
         <main className='verification-container '>
             <div className='d-flex align-items-start flex-column w-100 px-5'>
                 <h3>Reset password</h3>
                 <figcaption>Input and confirm new password</figcaption>
             </div>
-            <form className='mt-4'>
-                <input className='input-field' type='password' placeholder='Enter new password' />
-                <input className='input-field' type='password' placeholder='Confirm new password' />
-                <button className='form-btn' style={{width: '80%'}}>Update password</button>
+            <form className='mt-4' onSubmit={handleUpdatePassword}>
+                <input
+                    className='input-field'
+                    type='password'
+                    placeholder='Enter new password'
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <input
+                    className='input-field'
+                    type='password'
+                    placeholder='Confirm new password'
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button className='form-btn' style={{ width: '80%' }} type='submit'>
+                    Update password
+                </button>
             </form>
             <div className='pt-3 d-flex w-100 align-items-start px-5'>
                 <Link to="/">Click here to go back</Link>
             </div>
         </main>
-    )
-}
+    );
+};
 
-export default ResetPassword
+export default ResetPassword;
--- src/Pages/VerifyOTP.jsx
+++ src/Pages/VerifyOTP.jsx
@@ -1,20 +1,45 @@
 import React, {useState} from 'react'
 import "./pagesStyle.css"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const VerifyOTP = () => {
     const [otp, setOTP] = useState(''); // State to store the user's email
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleVerifyOTP = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/Components/verifying-OTP', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ otp }),
+            });
+
+            const data = await response.json();
+            setMessage(data.message);
+            navigate('/reset-password')
+        } catch (error) {
+            console.error('Error:', error);
+            setMessage('Internal server error.');
+        }
+    };
+
 
     return (
         <main className='verification-container '>
             <div className='d-flex align-items-start flex-column w-100 px-5'>
                 <h3>OTP Verification</h3>
-                <figcaption>Verify otp code to reset password</figcaption>
+                <figcaption>Verify OTP code to reset password</figcaption>
             </div>
-            <form className='mt-4' >
+            <form className='mt-4' onSubmit={handleVerifyOTP}>
                 <input className='input-field' type='number' name='otp' value={otp} placeholder='Enter OTP Code' onChange={(e) => setOTP(e.target.value)} />
-                <button type='submit' className='form-btn' style={{width: '80%'}}>Verify OTP</button>
+                <button type='submit' className='form-btn' style={{ width: '80%' }}>Verify OTP</button>
             </form>
+            {message && <p className="pt-3">{message}</p>}
             <span className='pt-3 d-flex align-items-center gap-2 w-100 px-5'>Want to go back?<Link to='/' className='text-decoration-none'>Click here</Link></span>
         </main>
     )
