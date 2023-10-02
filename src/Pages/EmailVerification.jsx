50bbe329460e54918cf26794a5d4859641273133
Joshua <lab.joshacc@gmail.com>
2023-10-03 01:46:49 +0800

    Revert "Just need to optimize the update info"
    This reverts commit 0e96c7b164740c7706a33c93af0f5f3b766a4233.

--- src/Pages/EmailVerification.jsx
+++ src/Pages/EmailVerification.jsx
@@ -1,35 +1,9 @@
 import React, {useState} from 'react'
 import "./pagesStyle.css"
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const EmailVerification = () => {
-    const [userEmail, setUserEmail] = useState('');
-    const Navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        try {
-          const response = await fetch('http://localhost:5000/Components/forgotpassword', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            body: JSON.stringify({ email: userEmail }),
-          });
-    
-          if (response.ok) {
-            const responseData = await response.json();
-            console.log(responseData)
-            const uID = responseData.userID
-            localStorage.setItem('userID', uID)
-            alert(responseData.message)
-            Navigate('/otp-verification')
-          } 
-        } catch (error) {
-          alert(error)
-        }
-      };
+    const [userEmail, setUserEmail] = useState(''); // State to store the user's email
 
     return (
         <main className='verification-container '>
@@ -37,13 +11,8 @@ const EmailVerification = () => {
                 <h3>Forgot password</h3>
                 <figcaption>Verify email to reset password</figcaption>
             </div>
-            <form className='mt-4' onSubmit={handleSubmit}>
-                <input 
-                className='input-field' 
-                type='email' name='email' 
-                value={userEmail} 
-                placeholder='Enter you email here' 
-                onChange={(e) => setUserEmail(e.target.value)} />
+            <form className='mt-4' >
+                <input className='input-field' type='email' name='email' value={userEmail} placeholder='Enter you email here' onChange={(e) => setUserEmail(e.target.value)} />
                 <button type='submit' className='form-btn' style={{width: '80%'}}>Verify Email</button>
             </form>
             <span className='pt-3 d-flex align-items-center gap-2 w-100 px-5'>Want to go back?<Link to='/' className='text-decoration-none'>Click here</Link></span>
